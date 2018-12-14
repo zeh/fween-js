8568b162d0a25285f13be2205650f62d3c853f90
Zeh Fernando <zeh@zehfernando.com>
2018-12-13 22:55:09 -0500

    Test: added basic test suites

---
+++ test/unit/Fween-test-es5.js
@@ -0,0 +1,7 @@
+var Fween = require("./../../dist/Fween.umd");
+
+describe("Fween (ES5)", function() {
+	test("is a class", function() {
+		expect(SimpleSignal).not.toBe("function");
+	});
+});
---
+++ test/unit/Fween-test-es6.js
@@ -0,0 +1,17 @@
+import Fween from "./../../dist/Fween.umd";
+
+const obj = {};
+
+describe("Fween (ES6)", () => {
+	test("is a class", function() {
+		expect(SimpleSignal).not.toBe("function");
+	});
+
+	test("can be instantiated", () => {
+		expect(Fween.use(obj)).toBeDefined();
+
+		//Fween.use(this).to({ pressedPhase: 1}, 0.2, Easing.quadOut).play();
+		//Fween.use(this).to({ pressedPhase: 0}, 0.3, Easing.backOutWith(2)).play();
+		//Fween.use(this).to({ alpha: 0}, 1).call(resolve).play();
+	});
+});
