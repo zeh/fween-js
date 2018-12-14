f1b86563a173d61be7f4d23225b11170c7aded18
Zeh Fernando <zeh@work.co>
2018-12-14 17:04:05 -0500

    Chore: updated node test versions

--- rollup.config.js
+++ rollup.config.js
@@ -5,10 +5,10 @@ import pkg from "./package.json";
 export default {
 	input: "src/Fween.ts",
 	output: [
-		{ name: "fween", sourcemap: true, file: pkg.module, format: "es" },
+		{ name: "fween", sourcemap: false, file: pkg.module, format: "es" },
 		{
 			name: "Fween",
-			sourcemap: true,
+			sourcemap: false,
 			file: pkg.main,
 			format: "umd",
 			footer: "module.exports.default = module.exports; // Terrible injection just so it works regardless of how it's required\n"
