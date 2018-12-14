6d5b8c05db05d509b7e01bb4cfb7fa18160ee501
Zeh Fernando <zeh@work.co>
2018-12-14 18:49:10 -0500

    Test: ES6 tests were expanded

--- test/unit/Fween-test-es6.js
+++ test/unit/Fween-test-es6.js
@@ -2,7 +2,16 @@ import Fween, { Easing } from "./../../dist/Fween.umd";
 import { enableTimeMocks, timeTravel, disableTimeMocks } from "./utils/timeMock";
 
 describe("Fween (ES6)", () => {
-	test("is a class", function() {
+	beforeEach(() => {
+		enableTimeMocks();
+		Fween.resetTicker();
+	});
+
+	afterEach(() => {
+		disableTimeMocks();
+	})
+
+	test("is a module", function() {
 		expect(Fween).not.toBe("function");
 	});
 
@@ -12,68 +21,52 @@ describe("Fween (ES6)", () => {
 	});
 
 	test("can update an object", () => {
-		enableTimeMocks();
-
 		const obj = { value: 0 };
-		const fween = Fween.use(obj);
 		expect(obj.value).toBe(0);
-		fween.to({value: 1}, 0.1).play();
+		Fween.use(obj).to({value: 1}, 0.1).play();
 		timeTravel(200);
 		expect(obj.value).toBe(1);
-		fween.to({value: 0}, 0.1).play();
+		Fween.use(obj).to({value: 0}, 0.1).play();
 		timeTravel(200);
 		expect(obj.value).toBe(0);
-		fween.to({value: 2}, 0.1).play();
+		Fween.use(obj).to({value: 2}, 0.1).play();
 		timeTravel(200);
 		expect(obj.value).toBe(2);
-
-		disableTimeMocks();
 	});
 
 	test("can update an object, easing", () => {
-		enableTimeMocks();
-
 		const obj = { value: 0 };
-		const fween = Fween.use(obj);
 		expect(obj.value).toBe(0);
-		fween.to({value: 1}, 0.1, Easing.quadOut).play();
+		Fween.use(obj).to({value: 1}, 0.1, Easing.quadOut).play();
 		timeTravel(200);
 		expect(obj.value).toBe(1);
-
-		disableTimeMocks();
 	});
 
 	test("can call a function", () => {
-		enableTimeMocks();
-
 		const fn1 = jest.fn();
 		const fn2 = jest.fn();
 
 		const obj = { value: 0 };
-		const fween = Fween.use(obj);
 		expect(obj.value).toBe(0);
-		fween.call(fn1).to({value: 1}, 0.1).call(fn2).play();
+		Fween.use(obj).call(fn1).to({value: 1}, 0.1).call(fn2).play();
 
 		expect(fn1).not.toBeCalled();
 		expect(fn2).not.toBeCalled();
 
 		timeTravel(50);
 
-		expect(fn1).not.toBeCalled();
-		expect(fn2).not.toBeCalled();
-
-		timeTravel(100);
-
 		expect(fn1).toHaveBeenCalledTimes(1);
 		expect(fn2).not.toBeCalled();
 
-		timeTravel(100);
+		timeTravel(60);
 
 		expect(fn1).toHaveBeenCalledTimes(1);
 		expect(fn2).toHaveBeenCalledTimes(1);
 
 		expect(obj.value).toBe(1);
-
-		disableTimeMocks();
 	});
+
+	// TODO:
+	// .wait()
+	// .use(setter)
 });
